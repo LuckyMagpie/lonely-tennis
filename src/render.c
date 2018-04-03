571cb5828e4f42ef21ef8b176e73fed07406d0ce
LuckyMagpie <diego.sse7@gmail.com>
2018-04-03 17:10:47 -0500

    Feat[render]: Add functions to render all objects in the world

--- src/render.c
+++ src/render.c
@@ -1,14 +1,22 @@
+#include <stdarg.h>
+
 #define GL3_PROTOTYPES 1
 #include <GL/glew.h>
 #include <SDL2/SDL.h>
 #include <cglm/cglm.h>
 
+#include "graphics.h"
 #include "world.h"
+#include "utils/vector.h"
 #include "render.h"
 
 void render_world(world_t* world, graphics_t* graphics)
 {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glUseProgram(graphics->program_id);
+
+    vector_foreach(world->world_objects, &do_render_call, graphics);
+
     SDL_GL_SwapWindow(graphics->window);
 }
 
@@ -42,4 +50,21 @@ void render_vertex_objects_free(world_object_t* object)
 {
     glDeleteBuffers(3, object->vbos);
     glDeleteVertexArrays(1, &object->vao);
+}
+
+void render_generic_object_draw(world_object_t* object, graphics_t* graphics)
+{
+    glBindVertexArray(object->vao);
+
+    mat4 MVP;
+    glm_mat4_mul(graphics->vp, object->model, MVP);
+    glUniformMatrix4fv(graphics->program_mvp_id, 1, GL_FALSE, MVP[0]);
+
+    glDrawArrays(GL_TRIANGLES, 0, object->vertices->size/3);
+}
+
+void do_render_call(void* object, va_list ap)
+{
+    graphics_t* graphics = va_arg(ap, graphics_t*);
+    ((world_object_t*)object)->do_render(object, graphics);
 }
--- src/render.h
+++ src/render.h
@@ -7,5 +7,7 @@
 void render_world(world_t* world, graphics_t* graphics);
 void render_vertex_objects_init(world_object_t* object);
 void render_vertex_objects_free(world_object_t* object);
+void render_generic_object_draw(world_object_t* object, graphics_t* graphics);
+void do_render_call(void* object, va_list ap);
 
 #endif /* ifndef RENDER_H */
