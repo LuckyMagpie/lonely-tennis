001555943ca868b6bdff575b41ab8fdb28f65c76
LuckyMagpie <diego.sse7@gmail.com>
2018-04-14 01:06:57 -0500

    Test[world]: Add world timer unit tests

--- test/test_world.c
+++ test/test_world.c
@@ -1,3 +1,5 @@
+#include <unistd.h>
+
 #include <check.h>
 
 #include "world.h"
@@ -14,13 +16,44 @@ START_TEST(test_world_init)
 }
 END_TEST
 
+START_TEST(test_world_start_timer)
+{
+    world_t* world = world_init();
+    world_start_timer(world);
+
+    ck_assert_msg(world->start_timer > 0, "Expected start_timer to be initiated got %d", world->start_timer);
+
+    world_free(world);
+}
+END_TEST
+
+START_TEST(test_world_current_delta_time)
+{
+    world_t* world = world_init();
+    world_start_timer(world);
+    sleep(1);
+
+    double delta = world_current_delta_time(world);
+
+    ck_assert_msg(delta >= 0.9 && delta <= 1.1 , "Expected delta to around 1 second got %f", delta);
+
+    world_free(world);
+}
+END_TEST
+
 Suite* world_suite()
 {
     Suite* suite = suite_create("world");
+
     TCase* tc1 = tcase_create("test_world_init");
     tcase_add_test(tc1, test_world_init);
 
+    TCase* tc2 = tcase_create("test_world_start_timer");
+    tcase_add_test(tc2, test_world_start_timer);
+    tcase_add_test(tc2, test_world_current_delta_time);
+
     suite_add_tcase(suite, tc1);
+    suite_add_tcase(suite, tc2);
     return suite;
 }
 
