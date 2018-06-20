f972fa354ed6794032b0ba23899be0f5fadbe0d1
LuckyMagpie <diego.sse7@gmail.com>
2018-06-20 00:50:51 -0500

    Feat[engine/render]: Load and draw textures for world objects

--- src/engine/render.c
+++ src/engine/render.c
@@ -3,6 +3,7 @@
 #define GL3_PROTOTYPES 1
 #include <GL/glew.h>
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
 #include <cglm/cglm.h>
 
 #include "graphics.h"
@@ -17,7 +18,6 @@ void render_world(world_t* world, graphics_t* graphics)
 
     vector_foreach(world->world_objects, &world_object_fn_render_call, graphics);
 
-    glBindVertexArray(0);
     glUseProgram(0);
 
     SDL_GL_SwapWindow(graphics->window);
@@ -49,19 +49,55 @@ void render_vertex_objects_init(world_object_t* wobj)
     glBindBuffer(GL_ARRAY_BUFFER, 0);
 }
 
+void render_texture_init(world_object_t* wobj, char* texture_path)
+{
+    SDL_Surface* texture = IMG_Load(texture_path);
+
+    if (!texture) {
+        fprintf(stderr, "%s:Unable to load texture, using default: %s\n", __func__, SDL_GetError());
+        texture = IMG_Load(DEFAULT_TEXTURE);
+    }
+
+    glGenTextures(1, &wobj->texture_id);
+    glBindTexture(GL_TEXTURE_2D, wobj->texture_id);
+
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, texture->w, texture->h, 0, GL_RGB, GL_UNSIGNED_BYTE, texture->pixels);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+    glGenerateMipmap(GL_TEXTURE_2D);
+
+    SDL_FreeSurface(texture);
+    glBindTexture(GL_TEXTURE_2D, 0);
+}
+
 void render_vertex_objects_free(world_object_t* wobj)
 {
     glDeleteBuffers(3, wobj->vbos);
     glDeleteVertexArrays(1, &wobj->vao);
 }
 
+void render_texture_free(world_object_t* wobj)
+{
+    glDeleteTextures(1, &wobj->texture_id);
+}
+
 void render_generic_object_draw(world_object_t* wobj, graphics_t* graphics)
 {
     glBindVertexArray(wobj->vao);
+
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, wobj->texture_id);
+    glUniform1i(graphics->program_texture_sampler_id, GL_TEXTURE0);
 
     mat4 MVP;
     glm_mat4_mul(graphics->vp, wobj->model_matrix, MVP);
     glUniformMatrix4fv(graphics->program_mvp_id, 1, GL_FALSE, MVP[0]);
 
     glDrawArrays(GL_TRIANGLES, 0, wobj->vertices->size / 3);
+
+    glBindTexture(GL_TEXTURE_2D, 0);
+    glBindVertexArray(0);
 }
--- src/engine/render.h
+++ src/engine/render.h
@@ -4,9 +4,13 @@
 #include "graphics.h"
 #include "world.h"
 
+static const char* DEFAULT_TEXTURE = "assets/default_texture.png";
+
 void render_world(world_t* world, graphics_t* graphics);
 void render_vertex_objects_init(world_object_t* wobj);
+void render_texture_init(world_object_t* wobj, char* texture_path);
 void render_vertex_objects_free(world_object_t* wobj);
+void render_texture_free(world_object_t* wobj);
 void render_generic_object_draw(world_object_t* wobj, graphics_t* graphics);
 
 #endif /* ifndef RENDER_H */
