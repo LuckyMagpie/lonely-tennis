b998091ef97d522ac7b86da2b0e124068dfe9c24
LuckyMagpie <diego.sse7@gmail.com>
2018-07-23 01:28:38 -0500

    Feat[engine/context]: Add event store and event dispatch/listener system.
    The event store takes SDL_EventTypes to make a hashtable of dynamic
    arrays where a callback and its target are stored.
    
    For now, mouse events are the only ones that have a target resolver
    function. Other events default to NULL (no target or global) since
    there's no way to tell what the actual target is. Maybe someday in the
    future, there can be target resolution for keyboard events or some other
    ones.

--- src/engine/context.c
+++ src/engine/context.c
@@ -1,16 +1,90 @@
+#include <stdarg.h>
 #include <stdbool.h>
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-#include "utils/enum_hashtable.h"
+#include <SDL2/SDL.h>
+#include <cglm/cglm.h>
+
 #include "context.h"
+#include "engine/colission.h"
 #include "graphics.h"
+#include "utils/enum_hashtable.h"
+#include "utils/vector.h"
 #include "world.h"
 
-static void context_store_value_free(enum_hashtable_value_t* value)
+static void context_state_callback_store_value_free(enum_hashtable_value_t* value)
 {
     free(value->data);
     free(value);
+}
+
+static void context_event_store_value_free(enum_hashtable_value_t* value)
+{
+    vector_foreach(value->data, &vector_generic_item_free);
+    vector_free(value->data);
+    free(value);
+}
+
+static void context_event_store_fire_event(void* obj, va_list ap)
+{
+    event_listener_t* listener = obj;
+    context_t* ctx = va_arg(ap, context_t*);
+    SDL_Event event = va_arg(ap, SDL_Event);
+    void* target = va_arg(ap, void*);
+
+    if (target == listener->target) {
+        listener->handler_fn(target, event, ctx);
+    }
+}
+
+static void context_event_store_dispatch(context_t* ctx, SDL_Event event, void* target)
+{
+    enum_hashtable_value_t* listener_store = enum_hashtable_get(ctx->event_store, event.type);
+    if (listener_store) {
+        vector_foreach(listener_store->data, &context_event_store_fire_event, ctx, event, target);
+    }
+}
+
+static void context_find_mouse_target(void* obj, va_list ap)
+{
+    world_object_t* wobj = obj;
+    void** target = va_arg(ap, void**);
+    float* camera_position = va_arg(ap, float*);
+    float* click_normal = va_arg(ap, float*);
+
+    if (colission_test_raycasting(camera_position, click_normal, wobj->bounding_volume)) {
+        if (*target) {
+            float current_target_distance = colission_query_distance2_bounding_volume(camera_position, ((world_object_t*)*target)->bounding_volume);
+            float new_target_distance = colission_query_distance2_bounding_volume(camera_position, wobj->bounding_volume);
+            *target = new_target_distance < current_target_distance? wobj : *target;
+        } else {
+            *target = wobj;
+        }
+    }
+}
+
+void* context_get_mouse_target(context_t* ctx, int mouse_pos_x, int mouse_pos_y)
+{
+    int window_width;
+    int window_height;
+    SDL_GetWindowSize(ctx->graphics->window, &window_width, &window_height);
+
+    vec3 click_vector = { mouse_pos_x, mouse_pos_y, 1.0f };
+    vec4 viewport = { 0, window_height, window_width, -window_height };
+    vec3 unprojected_click_vector;
+
+    glm_unproject(click_vector, ctx->graphics->vp, viewport, unprojected_click_vector);
+
+    vec3 click_normal;
+    glm_vec_sub(unprojected_click_vector, ctx->graphics->camera_position, click_normal);
+    glm_vec_normalize(click_normal);
+
+    void* target = NULL;
+
+    vector_foreach(ctx->world->world_objects, &context_find_mouse_target, &target, ctx->graphics->camera_position, click_normal);
+
+    return target;
 }
 
 context_t* context_init(world_t* world, graphics_t* graphics)
@@ -19,8 +93,8 @@ context_t* context_init(world_t* world, graphics_t* graphics)
     ctx->current_state = IN_START_MENU;
     ctx->world = world;
     ctx->graphics = graphics;
-    ctx->event_store = enum_hashtable_init(26, &context_store_value_free);
-    ctx->state_callback_store = enum_hashtable_init(5, &context_store_value_free);
+    ctx->event_store = enum_hashtable_init(26, &context_event_store_value_free);
+    ctx->state_callback_store = enum_hashtable_init(5, &context_state_callback_store_value_free);
 
     return ctx;
 }
@@ -42,6 +116,43 @@ void context_set_state_callback(context_t* ctx, state_t state, void (*callback)(
         state_callback->callback_fn = callback;
 
         enum_hashtable_put(ctx->state_callback_store, state, state_callback);
+    }
+}
+
+void context_event_store_add_listener(context_t* ctx, SDL_EventType type, void* target, void (*callback_fn)(void* target, SDL_Event event, context_t* ctx))
+{
+    enum_hashtable_value_t* listener_store = enum_hashtable_get(ctx->event_store, type);
+    if (!listener_store) {
+        vector_t* store = vector_init();
+        enum_hashtable_put(ctx->event_store, type, store);
+        listener_store = enum_hashtable_get(ctx->event_store, type);
+    }
+
+    event_listener_t* event_listener = malloc(sizeof(event_listener_t));
+    event_listener->handler_fn = callback_fn;
+    event_listener->target = target;
+
+    vector_push_back(listener_store->data, event_listener);
+}
+
+void context_events_listen(context_t* ctx)
+{
+    SDL_Event event;
+    while (SDL_PollEvent(&event)) {
+        switch (event.type) {
+        case SDL_MOUSEBUTTONDOWN:
+        case SDL_MOUSEBUTTONUP:;
+            void* mouse_button_target = context_get_mouse_target(ctx, event.button.x, event.button.y);
+            context_event_store_dispatch(ctx, event, mouse_button_target);
+            break;
+        case SDL_MOUSEMOTION:;
+            void* mouse_motion_target = context_get_mouse_target(ctx, event.motion.x, event.motion.y);
+            context_event_store_dispatch(ctx, event, mouse_motion_target);
+            break;
+        default:
+            context_event_store_dispatch(ctx, event, NULL);
+            break;
+        }
     }
 }
 
--- src/engine/context.h
+++ src/engine/context.h
@@ -1,8 +1,10 @@
 #ifndef CONTEXT_H
 #define CONTEXT_H
 
-#include "utils/enum_hashtable.h"
+#include <SDL2/SDL.h>
+
 #include "graphics.h"
+#include "utils/enum_hashtable.h"
 #include "world.h"
 
 typedef enum {
@@ -26,8 +28,16 @@ typedef struct state_callback_t {
     void (*callback_fn)(context_t*);
 } state_callback_t;
 
+typedef struct event_listener_t {
+    void* target;
+    void (*handler_fn)(void* target, SDL_Event event, context_t* ctx);
+} event_listener_t;
+
 context_t* context_init(world_t* world, graphics_t* graphics);
 void context_set_state_callback(context_t* ctx, state_t state, void (*callback)(context_t*));
+void context_event_store_add_listener(context_t* ctx, SDL_EventType type, void* target, void (*callback_fn)(void* target, SDL_Event event, context_t* ctx));
+void* context_get_mouse_target(context_t* ctx, int mouse_pos_x, int mouse_pos_y);
+void context_events_listen(context_t* ctx);
 void context_run(context_t* ctx);
 void context_free(context_t* cxt);
 
