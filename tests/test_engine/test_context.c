432ba1c6de19485de1eefa941aff06f6ffc3b043
LuckyMagpie <diego.sse7@gmail.com>
2018-07-27 01:34:02 -0500

    Test[engine/context]: Add mouse get target unit tests

--- tests/test_engine/test_context.c
+++ tests/test_engine/test_context.c
@@ -5,6 +5,7 @@
 #include "engine/world.h"
 #include "fixtures.h"
 #include "utils/enum_hashtable.h"
+#include "utils/vector.h"
 
 static int counter = 0;
 
@@ -79,6 +80,55 @@ START_TEST(test_context_set_state_callback)
 }
 END_TEST
 
+START_TEST(test_context_get_mouse_target)
+{
+    vec3 sphere_scale = { 1.0f, 1.0f, 1.0f };
+    float sphere_rotate_angle = 0.0f;
+    vec3 sphere_rotate_axis = { 0.0f, 0.0f, 0.0f };
+    vec3 sphere_translate = { 0.0f, 0.0f, 0.0f };
+    vec3 sphere_velocity = { 0.0f, 0.0f, 0.0f };
+
+    world_object_t* wobj = world_object_test_init(sphere_scale, sphere_rotate_angle, sphere_rotate_axis, sphere_translate, sphere_velocity);
+
+    world_t* world = world_init();
+    graphics_t* graphics = graphics_test_init();
+    vector_push_back(world->world_objects, wobj);
+    context_t* ctx = context_init(world, graphics);
+
+    void* target = context_get_mouse_target(ctx, 5, 5);
+
+    ck_assert_msg(target == wobj, "expected mouse target to be sphere in the middle of the screen");
+
+    context_free(ctx);
+}
+END_TEST
+
+START_TEST(test_context_get_mouse_target_overlap)
+{
+    vec3 sphere_scale = { 1.0f, 1.0f, 1.0f };
+    vec3 big_sphere_scale = { 10.0f, 10.0f, 10.0f };
+    float sphere_rotate_angle = 0.0f;
+    vec3 sphere_rotate_axis = { 0.0f, 0.0f, 0.0f };
+    vec3 sphere_translate = { 0.0f, 0.0f, 0.0f };
+    vec3 sphere_velocity = { 0.0f, 0.0f, 0.0f };
+
+    world_object_t* wobj = world_object_test_init(sphere_scale, sphere_rotate_angle, sphere_rotate_axis, sphere_translate, sphere_velocity);
+    world_object_t* wobj2 = world_object_test_init(big_sphere_scale, sphere_rotate_angle, sphere_rotate_axis, sphere_translate, sphere_velocity);
+
+    world_t* world = world_init();
+    graphics_t* graphics = graphics_test_init();
+    vector_push_back(world->world_objects, wobj);
+    vector_push_back(world->world_objects, wobj2);
+    context_t* ctx = context_init(world, graphics);
+
+    void* target = context_get_mouse_target(ctx, 5, 5);
+
+    ck_assert_msg(target == wobj2, "expected mouse target to be bigger sphere");
+
+    context_free(ctx);
+}
+END_TEST
+
 START_TEST(test_context_run)
 {
     world_t* world = world_init();
@@ -113,9 +163,14 @@ Suite* context_suite()
     TCase* tc3 = tcase_create("test_context_run");
     tcase_add_test(tc3, test_context_run);
 
+    TCase* tc4 = tcase_create("test_context_get_mouse_target");
+    tcase_add_test(tc4, test_context_get_mouse_target);
+    tcase_add_test(tc4, test_context_get_mouse_target_overlap);
+
     suite_add_tcase(suite, tc1);
     suite_add_tcase(suite, tc2);
     suite_add_tcase(suite, tc3);
+    suite_add_tcase(suite, tc4);
     return suite;
 }
 
